b83767dc5754fca301b4d12c49fc9199b9551d77
Yedidia_ <solomonyedidia@gmail.com>
2016-04-18 14:53:15 +0200

    Added files via upload

---
+++ Day11/ex00/ft_create_elem.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymukmar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/14 15:51:55 by ymukmar           #+#    #+#             */
+/*   Updated: 2016/04/14 16:54:53 by ymukmar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list  *list;
+
+	list = NULL;
+	list = malloc(sizeof(t_list));
+	if (list)
+	{
+		list->data = data;
+		list->next = NULL;
+	}
+	return (list);
+}
